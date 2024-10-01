817d9eee37f239eb85e81aa0daedab3b080f2fc0
Yi Li <yli22@mun.ca>
2024-09-30 22:52:53 -0230

    页面基础信息编写

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from './components/Header';
+import AboutMe from './components/aboutMe'; // 假设 AboutMe 是一个组件
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainPage from './components/mainPage';
+import Home from './components/home';
+import Projects from './components/projects';
+import Skills from './components/skill';
+import Contact from './components/contact';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-    </div>
+    <Router>
+      {/* 静态内容部分，将始终显示 */}
+      <div className="App">
+        <Header />
+
+        {/* 路由部分，将根据路径不同显示对应的组件 */}
+        <Routes>
+          <Route path="/aboutMe" element={<AboutMe />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* 其他路由 */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,85 @@
 import React from 'react';
+// import { Button } from 'antd';
+import { HomeOutlined, SolutionOutlined, MailOutlined, FundProjectionScreenOutlined,AlertOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+import { icons } from 'antd/es/image/PreviewGroup';
+import { useLocation, Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
-function Header() {
-  return (
-    <header>
-      <h1>My Personal Website</h1>
-      <nav>
-        <a href="#about">About</a>
-        <a href="#projects">Projects</a>
-        <a href="#contact">Contact</a>
-      </nav>
-    </header>
-  );
-}
+
+const items = [
+  {
+    label: (
+      <Link to={"/"}>
+        Home
+      </Link>
+    ),
+    key: 'Home',
+    icon: <HomeOutlined />,
+  },
+  {
+    label: (
+      <Link to={"/aboutMe"}>
+        About me
+      </Link>
+    ),
+    key: 'aboutMe',
+    icon: <SolutionOutlined />
+  },
+  {
+    label: (
+      <Link to={"/projects"}>
+        Projects
+      </Link>),
+    key: 'Projects',
+    icon: <FundProjectionScreenOutlined />,
+    
+  },
+  {
+    key: 'Skills',
+    label: (
+      <Link to={"/skills"}>
+        Skills
+      </Link>),
+    icon: <AlertOutlined />,
+  },
+  {
+    key: 'mail',
+    icon: <MailOutlined />,
+    label: (
+      <Link to={"/Contact"}>
+        Contact
+      </Link>),
+  },
+];
+
+const Header = () => {
+  const location = useLocation();
+  const [current, setCurrent] = useState('Home');
+
+    // useEffect to update the current state when the location changes
+    useEffect(() => {
+      if (location.pathname === '/aboutMe') {
+        console.log('aboutMe');
+        setCurrent('aboutMe');
+      } else if (location.pathname === '/Projects') {
+        setCurrent('Projects');
+      }else if (location.pathname === '/Skills') {
+        setCurrent('Skills');
+      }else if (location.pathname === '/Contact') {
+        setCurrent('mail');
+      }
+    }, [location]);
+
+  const onClick = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+  return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+};
+
+
+
+
 
 export default Header;
